0a68de3361528ff79eed09af09ea28da8a1c42b3
dsaswal <35374922+dsaswal@users.noreply.github.com>
2023-06-14 06:50:53 +0200

    added entity and repo

---
+++ src/main/java/dsa/personal/notespsqlv04/Notes.java
@@ -0,0 +1,55 @@
+package dsa.personal.notespsqlv04;
+
+public class Notes {
+	
+	private long id;
+	private String title;
+	private String note;
+	private long timestamp;
+	
+	public Notes(long id, String title, String note, long timestamp) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.note = note;
+		this.timestamp = timestamp;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public long getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	@Override
+	public String toString() {
+		return "Notes [id=" + id + ", title=" + title + ", note=" + note + ", timestamp=" + timestamp + "]";
+	}
+
+}
